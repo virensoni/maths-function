fdea19335a626552cfe476c4b4474ad248b838d6
Viren Paresh Soni <P14176727@GWHXX581LIN019W.lec-admin.dmu.ac.uk>
2016-02-15 14:27:51 +0000

    this is when the additional function was added

--- maths_function.c
+++ maths_function.c
@@ -27,6 +27,40 @@ int main()
     remainder=dividend%divisor;          /* Computes remainder */
     printf("Quotient = %d\n",quotient);
     printf("Remainder = %d\n",remainder);
+
+
+int maximum=0,minimum=1,number,sum=0;
+int count=0;
+float average;
+
+
+while(1)
+{
+cout<<"Enter a number(-1 to quit): "; cin>>number;    
+
+if(number==-1)
+    break;  // enough asking for numbers
+    
+else if(number<1 || number>100)
+    {cout<<"\nOut of range.Enter between 1-100.\n";continue;} //invalid input,do again
+
+
+maximum=(number>maximum)?number:maximum; //conditional statement,
+                                        //if entered number is greater than our max,its new max
+
+minimum=(number<minimum)?number:minimum;//condiitonal statement,
+                                        //if entered number is smaller than our min,its new min
+sum+=number; //add to sum    
+count++;     // increment count
+}
+
+average=float(sum)/float(count);
+
+cout<<"\nYou entered " << count << " numbers."<<endl
+    <<"Their average is : "<<average<<endl
+    <<"Max number is : "<<maximum<<endl
+    <<"Min number is : "<<minimum;  
+return 0;
  
    return 0;
 }
